ae96c587fa7e9a4f01dcc83f3c677e19a3be3202
jmayorga94 <javi.mayorga1994@gmail.com>
2021-04-06 11:37:28 -0600

    Iniatiate project

---
+++ src/main/java/com/azure/keyvaultdemo/controllers/keys.java
@@ -0,0 +1,17 @@
+package com.azure.keyvaultdemo.controllers;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class keys {
+
+	@Value("${testconnection}")
+	private String testConnection = "defaultValue\n";
+	  
+	@GetMapping("get")
+    public String get() {
+        return testConnection;
+    }
+}
